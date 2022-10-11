faf85f7d3fbb29ffc3f90d427610c75c601afb7f
MOHANKUMAR-IT <mohankumar.it.2001@gmail.com>
2022-10-11 16:01:21 +0530

    front end for landing page goin on

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
+import time
 
 app = Flask(__name__)
 
@@ -10,52 +11,67 @@ app.secret_key = 'StrongPixel1090!'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'CustomerCare'
+app.config['MYSQL_DB'] = 'customer_care_registry'
 
 mysql = MySQL(app)
 
 @app.route('/')
+def landing_page():
+    return render_template("index.html")
 @app.route('/login', methods =['GET', 'POST'])
 def login():
     msg = ''
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-	    username = request.form['username']
+    print("came in")
+    if request.method == 'POST' and 'email_id' in request.form and 'password' in request.form:
+        # print(request.form)
+        email_id = request.form['email_id']
         password = request.form['password']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM user_accounts WHERE username = % s AND password = % s', (username, password, ))
+        cursor.execute('SELECT * FROM user_accounts WHERE email_id LIKE %s AND password LIKE %s', [email_id, password])
         account = cursor.fetchone()
+        # print(account)
         if account:
             session['loggedin'] = True
-            session['id'] = account['id']
-            session['username'] = account['username']
+            session['session_id'] = hash(account['email_id']+str(hash(account['password']+str(time.time()))))
+            session['email_id'] = account['email_id']
+            session['user_name'] = account['user_name']
             msg = 'Logged in successfully !'
             return render_template('index.html', msg = msg)
         else:
             msg = 'Incorrect username / password !'
-        return render_template('login.html', msg = msg)
+    return render_template('login.html', msg = msg)
 
 @app.route('/register', methods =['GET', 'POST'])
 def register():
-	msg = ''
-	if request.method == 'POST' and 'first_name' in request.form and 'password' in request.form and 'email_id' in request.form and 'last_name' in request.form :
-		first_name = request.form['first_name']
+    msg = ''
+    print(list(request.form))
+    if request.method == 'POST' and 'first_name' in request.form and 'password' in request.form and 'email_id' in request.form and 'last_name' in request.form and 'pno' in request.form and 'user_name' in request.form :
+        first_name = request.form['first_name']
         last_name = request.form['last_name']
-		password = request.form['password']
-		email_id = request.form['email_id']
+        password = request.form['password']
+        email_id = request.form['email_id']
+        user_name = request.form['user_name']
+        pno = request.form['pno']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        print(cursor)
+        cursor.execute('SELECT * FROM user_accounts WHERE email_id LIKE %s', [email_id])
+        account = cursor.fetchone()
+        if account: 
+            msg = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email_id):
+            msg = 'Invalid email address !'
+        elif not first_name or not last_name or not password or not email_id or not pno or not user_name:
+            msg = 'Please fill out the form !'
+        else:
+            cursor.execute('INSERT INTO user_accounts VALUES (% s, % s, % s , % s , % s , % s )', ( email_id ,user_name,password,pno,first_name,last_name))
+            mysql.connection.commit()
+            msg = 'You have successfully registered !'
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    return render_template('register.html', msg = msg)
 
-		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-		cursor.execute('SELECT * FROM user_accounts WHERE email_id = % s', (email_id, ))
-		account = cursor.fetchone()
-		if account:
-			msg = 'Account already exists !'
-		elif not re.match(r'[^@]+@[^@]+\.[^@]+', email_id):
-			msg = 'Invalid email address !'
-		elif not first_name or not last_name or not password or not email_id:
-			msg = 'Please fill out the form !'
-		else:
-			cursor.execute('INSERT INTO user_accounts VALUES (% s, % s, % s)', ( email_id ,password, ))
-			mysql.connection.commit()
-			msg = 'You have successfully registered !'
-	elif request.method == 'POST':
-		msg = 'Please fill out the form !'
-	return render_template('register.html', msg = msg)
+# app.run(use_reloader=True)
+if __name__ == '__main__':
+    from livereload import Server
+    server = Server(app.wsgi_app)
+    server.serve(host = '0.0.0.0',port=5000)
