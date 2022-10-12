2c3f78b332d106c0960dad9281381472be5f02e5
MOHANKUMAR-IT <mohankumar.it.2001@gmail.com>
2022-10-12 23:20:54 +0530

    new file:   Lib/site-packages/pip/_internal/commands/__pycache__/list.cpython-310.pyc 	new file:   Lib/site-packages/pip/_vendor/rich/__pycache__/_windows_renderer.cpython-310.pyc 	modified:   app.py 	new file:   static/Drawable/division-pictures/arrow.png 	new file:   static/Drawable/division-pictures/issue-pc.png 	new file:   static/Drawable/division-pictures/man and women.jpg 	new file:   static/Drawable/division-pictures/man_and_women.png 	new file:   static/Drawable/division-pictures/two man one women.jpg 	new file:   static/Drawable/division-pictures/two_man_one_women.png 	new file:   static/Drawable/videos/Mountains.mp4 	new file:   static/javascript/navlink-activator.js 	modified:   static/styles/landing-page.css 	modified:   templates/index.html 	modified:   templates/login.html

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from tkinter.messagebox import NO
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
@@ -16,12 +17,29 @@ app.config['MYSQL_DB'] = 'customer_care_registry'
 mysql = MySQL(app)
 
 @app.route('/')
+@app.route('/index')
 def landing_page():
-    return render_template("index.html")
+    if(len(session)):
+        return render_template("index.html")
+    else:
+        redirect('/login')
+
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('session_id', None)
+    session.pop('user_name', None)
+    session.pop('first_name',None)
+    session.pop('last_name',None)
+    session.pop('email_id',None)
+    
+    return redirect(url_for('login'))
+
+
 @app.route('/login', methods =['GET', 'POST'])
 def login():
     msg = ''
-    print("came in")
+    # print("came in")
     if request.method == 'POST' and 'email_id' in request.form and 'password' in request.form:
         # print(request.form)
         email_id = request.form['email_id']
@@ -35,8 +53,9 @@ def login():
             session['session_id'] = hash(account['email_id']+str(hash(account['password']+str(time.time()))))
             session['email_id'] = account['email_id']
             session['user_name'] = account['user_name']
-            msg = 'Logged in successfully !'
-            return render_template('index.html', msg = msg)
+            session['first_name'] = account['first_name']
+            session['last_name'] = account['last_name']
+            return redirect('index')
         else:
             msg = 'Incorrect username / password !'
     return render_template('login.html', msg = msg)
@@ -44,7 +63,7 @@ def login():
 @app.route('/register', methods =['GET', 'POST'])
 def register():
     msg = ''
-    print(list(request.form))
+    # print(list(request.form))
     if request.method == 'POST' and 'first_name' in request.form and 'password' in request.form and 'email_id' in request.form and 'last_name' in request.form and 'pno' in request.form and 'user_name' in request.form :
         first_name = request.form['first_name']
         last_name = request.form['last_name']
@@ -53,7 +72,7 @@ def register():
         user_name = request.form['user_name']
         pno = request.form['pno']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        print(cursor)
+        # print(cursor)
         cursor.execute('SELECT * FROM user_accounts WHERE email_id LIKE %s', [email_id])
         account = cursor.fetchone()
         if account: 
@@ -66,12 +85,15 @@ def register():
             cursor.execute('INSERT INTO user_accounts VALUES (% s, % s, % s , % s , % s , % s )', ( email_id ,user_name,password,pno,first_name,last_name))
             mysql.connection.commit()
             msg = 'You have successfully registered !'
+            redirect('/login')
     elif request.method == 'POST':
         msg = 'Please fill out the form !'
     return render_template('register.html', msg = msg)
 
 # app.run(use_reloader=True)
 if __name__ == '__main__':
-    from livereload import Server
-    server = Server(app.wsgi_app)
-    server.serve(host = '0.0.0.0',port=5000)
+    # from livereload import Server
+    # server = Server(app.wsgi_app)
+    app.run(debug=True)
+    # server.serve(host = '0.0.0.0',port=5000)
+    
